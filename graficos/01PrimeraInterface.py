03e0273385035b87ee12a9fdb13c80526217ee7d
daniel <pardalaco@gmail.com>
2019-07-28 13:52:41 +0200

    Instal·lem Tkinter així: apt install python3-tk python3-pil.imagetk

--- graficos/01PrimeraInterface.py
+++ graficos/01PrimeraInterface.py
@@ -6,7 +6,7 @@ raiz.title("Ventana de pruebas")#Le pone nombre a la ventaqna y aparece en la pa
 
 raiz.resizable(0,0)#Esta variable es de tipo buleano (0,1), (True, False). El primero corresponde al ancho (width) y el segundo al alto (height) y le estamos diciendo que no se redimensione ni a lo ancho ni a lo alto (False, False)
 
-raiz.iconbitmap("slash.ico")#raiz.nombre del objeto(ruta del objeto)
+#raiz.iconbitmap("slash.ico")#raiz.nombre del objeto(ruta del objeto)
 
 raiz.geometry("650x350")# Esta funcion sirve para definir la ventana
 
